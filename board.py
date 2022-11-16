e4aafa125de005051e9dc2b89adedc1865055c8c
Fer Flores Vogt-Kielland <ferfloresvk@gmail.com>
2022-11-16 08:31:07 +0100

    create sudoku not working yet

--- board.py
+++ board.py
@@ -26,7 +26,7 @@ class SudokuBoard(tk.Tk):
         super().__init__()
         self.original_sudoku = normal_sudoku
         self.working_sudoku = self.original_sudoku.copy()
-        self.size_of_unit = 2
+        self.size_of_unit = 3
         self.spaces = {}
         self.counter = 0
         self.found_solutions = 0
@@ -36,46 +36,87 @@ class SudokuBoard(tk.Tk):
         ]
 
         self.create_hud()
-        self.create_blank_board(self.size_of_unit)
+        self.create_blank_board()
+        self.reset_board()
 
     def create_hud(self):
         hud = tk.Frame(self)
         message = tk.Label(hud, font=title_font, text=f"Let's play some Sudoku")
-        message.pack(fill=tk.X)
+        message.grid(row=0, columnspan=3)
+
         button_check_solution = tk.Button(
             master=hud,
             text="check solution",
             command=self.is_it_solved,
         )
-        button_check_solution.pack()
+        button_check_solution.grid(row=1, column=0)
+
         button_give_solution = tk.Button(
             master=hud,
-            text="give solution",
-            command=self.create_solution,
+            text="show no. solutions",
+            command=self.solution_message,
         )
-        button_give_solution.pack()
+        button_give_solution.grid(row=1, column=1)
+
+        button_create_solution = tk.Button(
+            master=hud,
+            text="give solution",
+            command=self.print_solution,
+        )
+        button_create_solution.grid(row=1, column=2)
+
+        button_reset = tk.Button(
+            master=hud,
+            text="reset",
+            command=self.reset_board,
+        )
+        button_reset.grid(row=2, column=0)
+
         button_create_sudoku = tk.Button(
             master=hud,
             text="create sudoku",
             command=self.create_sudoku,
         )
-        button_create_sudoku.pack()
+        button_create_sudoku.grid(row=2, column=1)
+
+        button_unique_sudoku = tk.Button(
+            master=hud,
+            text="unique sudoku",
+            command=self.unique_sudoku,
+        )
+        button_unique_sudoku.grid(row=2, column=2)
+
         hud.pack(fill=tk.X)
 
-    def create_blank_board(self, size):
-        self.working_sudoku = [[0 for x in range(size**2)] for x in range(size**2)]
+    def reset_board(self):
+        self.working_sudoku = [
+            [0 for x in range(self.size_of_unit**2)]
+            for x in range(self.size_of_unit**2)
+        ]
+        self.counter = 0
+        self.found_solutions = 0
+        for k, v in self.spaces.items():
+            x, y = v
+            self.update_board(k, "")
+            self.unique_solution[x][y] = self.working_sudoku[x][y]
+
+    def create_blank_board(self):
+        self.working_sudoku = [
+            [0 for x in range(self.size_of_unit**2)]
+            for x in range(self.size_of_unit**2)
+        ]
         board = tk.Frame(self)
         board.pack()
         self.color = cycle(colors)
 
-        for row in range(size**2):
-            if is_even(self.size_of_unit) and int(row % size) == 0:
+        for row in range(self.size_of_unit**2):
+            if is_even(self.size_of_unit) and int(row % self.size_of_unit) == 0:
                 bg_color = next(self.color)
-            elif not is_even(self.size_of_unit) and int(row % size) != 0:
+            elif not is_even(self.size_of_unit) and int(row % self.size_of_unit) != 0:
                 bg_color = next(self.color)
 
-            for col in range(size**2):
-                if int(col % size) == 0:
+            for col in range(self.size_of_unit**2):
+                if int(col % self.size_of_unit) == 0:
                     bg_color = next(self.color)
                 button = tk.Button(
                     master=board,
@@ -227,6 +268,28 @@ class SudokuBoard(tk.Tk):
                     return False
         return True
 
+    def give_solutions(self):
+
+        solved = self.check_for_solution()
+
+        while self.counter < MAX_TOTAL_CYCLES:
+            if solved:
+                self.counter += 1
+                self.found_solutions += 1
+                return
+            else:
+                for y in range(self.size_of_unit**2):
+                    for x in range(self.size_of_unit**2):
+                        if self.working_sudoku[y][x] == 0:
+                            for number in range(1, (self.size_of_unit**2) + 1):
+                                if self.check_valid_move(
+                                    self.working_sudoku, x, y, number
+                                ):
+                                    self.working_sudoku[y][x] = number
+                                    self.give_solutions()
+                                    self.working_sudoku[y][x] = 0
+                            return
+
     def solve_sudoku(self):
 
         solved = self.check_for_solution()
@@ -234,18 +297,14 @@ class SudokuBoard(tk.Tk):
         while self.counter < MAX_TOTAL_CYCLES:
             if solved:
                 self.counter += 1
-                if self.counter == 0:
-                    print("the sudoku entered is solved")
-                    return
-                else:
-                    self.found_solutions += 1
-                    if self.found_solutions == 1:
-                        for k, v in self.spaces.items():
-                            x, y = v
-                            self.update_board(k, self.working_sudoku[x][y])
-                            self.unique_solution[x][y] = self.working_sudoku[x][y]
-                    self.counter += 1
-                    return
+                self.found_solutions += 1
+                if self.found_solutions == 1:
+                    for k, v in self.spaces.items():
+                        x, y = v
+                        self.update_board(k, self.working_sudoku[x][y])
+                        self.unique_solution[x][y] = self.working_sudoku[x][y]
+                self.counter += 1
+                return
             else:
                 for y in range(self.size_of_unit**2):
                     for x in range(self.size_of_unit**2):
@@ -259,7 +318,14 @@ class SudokuBoard(tk.Tk):
                                     self.working_sudoku[y][x] = 0
                             return
 
+    def print_solution(self):
+        self.solve_sudoku()
+        self.working_sudoku = self.unique_solution
+
     def create_sudoku(self):
+        self.counter = 0
+        self.found_solutions = 0
+
         self.working_sudoku = [
             [0 for x in range(self.size_of_unit**2)]
             for x in range(self.size_of_unit**2)
@@ -271,33 +337,68 @@ class SudokuBoard(tk.Tk):
         self.solve_sudoku()
         self.working_sudoku = self.unique_solution
 
+        self.counter = 0
+        self.found_solutions = 0
+
+    def unique_sudoku(self):
+        self.counter = 0
+        self.found_solutions = 0
+        self.give_solutions()
+        for _ in range(int((self.size_of_unit**3) / 4)):
+            rand_x_coordinate = random.randint(1, (self.size_of_unit**2) + 1)
+            rand_y_coordinate = random.randint(1, (self.size_of_unit**2) + 1)
+            self.working_sudoku[rand_x_coordinate][rand_y_coordinate] = 0
+        self.give_solutions()
+
+        if self.found_solutions > 1:
+            rand_x_coordinate = random.randint(1, (self.size_of_unit**2) + 1)
+            rand_y_coordinate = random.randint(1, (self.size_of_unit**2) + 1)
+            self.working_sudoku[rand_x_coordinate][rand_y_coordinate] = 0
+            for row in self.working_sudoku:
+                print(row)
+            self.unique_sudoku()
+            return
+
     def display_message(self, message: str = ""):
         window = tk.Toplevel(self)
-        label = tk.Label(master=window, text=message)
+        # window.geometry("500x50")
+        label = tk.Label(master=window, text=message, font=title_font)
         label.pack()
 
-    def create_solution(self):
-        self.solve_sudoku()
+    def solution_message(self):
         self.working_sudoku = self.unique_solution
+        self.give_solutions()
+        if self.check_for_solution():
+            self.display_message(f"the sudoku is already solved")
 
-        if self.counter == 1:
-            self.display_message(f"the sudoku has one solution")
+        elif self.found_solutions == 1:
+            self.display_message(f"the sudoku has a unique solution")
+            self.counter = 0
+            self.found_solutions = 0
 
         elif self.counter == MAX_TOTAL_CYCLES:
             self.display_message(
-                f"the sudoku has at least {MAX_TOTAL_CYCLES} solutions"
+                f"the sudoku has at least {MAX_TOTAL_CYCLES} ways to be solved"
             )
+            self.counter = 0
+            self.found_solutions = 0
 
         elif self.counter > 1:
-            self.display_message(f"the sudoku has {self.found_solutions} solutions")
+            self.display_message(
+                f"the sudoku has {self.found_solutions} ways to be solved"
+            )
+            self.counter = 0
+            self.found_solutions = 0
 
         elif self.counter == 0:
             self.display_message(f"the sudoku has no solution")
+            self.counter = 0
+            self.found_solutions = 0
 
         else:
             self.display_message("check other options")
 
 
 if __name__ == "__main__":
-    a = SudokuBoard()
-    a.mainloop()
+    game = SudokuBoard()
+    game.mainloop()
